7e4bd2dd66995102f7c2a82bf10acb1298894189
qinyi-carrie <12mytan@gmail.com>
2021-07-20 20:49:30 +0800

    将智能决策树的流程走一遍开发基础项目流程

--- src/type.ts
+++ src/type.ts
@@ -2,6 +2,7 @@ import optionLibraries from "./optionLibraries";
 
 export type OptionKey = keyof typeof optionLibraries;
 
+
 export interface Flow {
   title: string;
   options: OptionKey[] | Record<any, OptionKey[]>;
